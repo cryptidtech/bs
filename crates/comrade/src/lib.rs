9ca8903c0c687813f80e0b594702d5c29e2decdf
Douglas Anderson <douganderson444@gmail.com>
2025-05-08 10:36:43 -0300

    WIP: Start to scaffold the main API
    Work in progress

--- crates/comrade/src/lib.rs
+++ crates/comrade/src/lib.rs
@@ -2,24 +2,41 @@
 //!
 //! It requires a wasm-component plugin to run. A reference implementation is
 //! provided in the `comrade-component` crate.
+mod error;
+
+use comrade_reference::Pairs;
 
 /// Opinionated entry API for using Comrade.
 /// Uses the comrade-component reference implementation by default,
 /// and wasm_component_layer for runtime. Either can be substituted
 /// with prefered alternatives as desired.
-mod api;
-
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub struct Comrade<C, P> {
+    lock: C,
+    unlock: P,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// API should be something like:
+//
+// let unlocked = Comrade::new(kvp_lock, kvp_unlock)
+//     .with_domain("/")
+//     .try_unlock(&unlock)?;
+//
+// let mut count = 0;
+//
+// for lock in locks {
+//     if let Some(Value::Success(ct)) = unlocked.try_lock(lock)? {
+//         count = ct;
+//         break;
+//     }
+// }
+//
+// where the args impl Pairs
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl<C: Pairs, P: Pairs> Comrade<C, P> {
+    pub fn new(lock: C, unlock: P) {}
+
+    pub fn try_unlock(script: &str) {
+        // something like:
+        // vm::run(script)
     }
 }
